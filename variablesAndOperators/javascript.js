5df07bc0149da0383658e8ad14086f7e607c0580
FlintJunior <137229417+FlintDHawtblaster@users.noreply.github.com>
2026-04-09 14:11:08 +0000

    Add a directory with code relating to adding Javascript to my HTML code and creating variables and using operators

---
+++ variablesAndOperators/javascript.js
@@ -0,0 +1,36 @@
+//PRINTING OUT 
+// let firstName = "Johnny";
+// let lastName = "Depp";
+
+// console.log(firstName);
+// console.log(lastName);
+
+//TRYING TO RE-ASSIGN VARIABLES
+// let age = 19;
+// console.log(age);
+
+// age = 55;
+// console.log(age);
+
+// const pi = 3.14;
+// pi = 10
+
+// console.log(pi);
+
+console.log(23 + 97);
+console.log(54 + 45 + 74 + 12 + 87 + 60);
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 15;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
